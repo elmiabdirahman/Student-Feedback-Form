b51f7e4a60170ba6f11a99c5b3ef37deba2586a0
Elmi Elmi <elmielmi522@gmail.com>
2019-12-14 13:34:43 -0600

    setup started page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,36 +1,25 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import { HashRouter as Router, Route } from 'react-router-dom';
+import StartPage from '../StartPage/StartPage';
 import './App.css';
 
 class App extends Component {
 
-  componentDidMount(){
-    console.log('in componentDidMount');
-    this.getFeedbacks();
-  }
-
-  getFeedbacks() {
-    axios({
-        method: `GET`,
-        url: `/feedback`
-    }).then((response)=>{
-        console.log('back from GET', response);
-        this.setState( response.data);  
-    }).catch((err)=>{
-        console.log('err getting student', err);  
-    })
-  }
 
 
   render() {
     return (
+      <Router>
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Feedback!</h1>
           <h4><i>Don't forget it!</i></h4>
         </header>
+        <h1>How are you feeling today?</h1>
+        <Route exact path="/" component={StartPage} />
         <br/>
       </div>
+      </Router>
     );
   }
 }
---
+++ src/components/StartPage/StartPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class StartPage extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello Everyone</h1>
+            </div>
+        );
+    }
+}
+
+export default StartPage;
